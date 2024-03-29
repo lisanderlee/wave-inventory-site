abd9700ac3d1177c5a382be57edbddd6a783f188
Lisandro <lisander.lee@gmail.com>
2024-03-28 22:40:16 -0700

    new colors

--- components/solution.js
+++ components/solution.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import POS from '@/public/Images/POS.svg'
 import BOX from '@/public/Images/Box.svg'
-
+import { CheckIcon } from '@heroicons/react/20/solid'
 import Handheld from '@/public/Images/handheld.svg'
 import Tag from '@/public/Images/Tag.svg'
 import { useTranslations } from 'next-intl'
@@ -31,27 +31,35 @@ export default function Solution() {
               <div className="flex flex-col max-w-3xl">
                 <div className="flex flex-col">
                   <div className="flex flex-col ">
-                    <h3 className="text-3xl flex font-bold text-indigo-600"> {t('features.tag.name')} </h3>
+                    <h3 className="text-5xl flex font-bold text-indigo-600"> {t('features.tag.name')} </h3>
                   </div>
-                  <p className=" text-md text-gray-500 mt-5 "> {t('features.tag.description')} </p>
+
+                  <ul className="mt-5 ml-5 text-lg text-gray-500">
+                    <li className="list-disc">{t('features.tag.line-1')}</li>
+                    <li className="list-disc">{t('features.tag.line-2')}</li>
+                    <li className="list-disc">{t('features.tag.line-3')}</li>
+                    <li className="list-disc	 ">{t('features.tag.line-4')}</li>
+                    <li className="list-disc	 ">{t('features.tag.line-5')}</li>
+                  </ul>
+                  {/* <p className=" text-md text-gray-500 mt-5 "> {t('features.tag.description')} </p> */}
                 </div>
                 <div className="flex flex-row mt-10 ">
                   <dl className="  text-gray-900 ">
                     <div className="flex flex-col  lg:items-center gap-y-3 ">
                       <VideoButton videoSelector="videos/tag.mp4" />
-                      <dt className="text-sm leading-6 text-indigo-600 font-bold ">{t('videocta')} </dt>
+                      <dt className="text-xs lg:text-sm lg:leading-6 leading-1  text-indigo-600 font-bold ">{t('videocta')} </dt>
                     </div>
                   </dl>
                   <dl className=" lg:ml-10 ml-5 text-gray-900 ">
                     <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
-                      <dt className="text-sm leading-6">{t('features.tag.stat-1-description')} </dt>
-                      <dd className="order-first text-3xl font-semibold tracking-tight">{t('features.tag.stat-1')}</dd>
+                      <dt className=" text-xs lg:text-sm    leading-4 leading-1">{t('features.tag.stat-1-description')} </dt>
+                      <dd className="order-first text-2xl lg:text-3xl font-semibold tracking-tight">{t('features.tag.stat-1')}</dd>
                     </div>
                   </dl>
                   <dl className="lg:ml-10 ml-5 text-gray-900 ">
-                    <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
-                      <dt className="text-sm leading-6">{t('features.tag.stat-2-description')} </dt>
-                      <dd className="order-first text-3xl font-semibold tracking-tight">{t('features.tag.stat-2')}</dd>
+                  <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
+                      <dt className=" text-xs lg:text-sm    leading-4 leading-1">{t('features.tag.stat-2-description')} </dt>
+                      <dd className="order-first text-2xl lg:text-3xl font-semibold tracking-tight">{t('features.tag.stat-2')}</dd>
                     </div>
                   </dl>
                 </div>
@@ -68,28 +76,34 @@ export default function Solution() {
               <div className="flex flex-col max-w-3xl">
                 <div className="flex flex-col">
                   <div className="flex flex-row">
-                    <h3 className="text-3xl font-bold text-indigo-600"> {t('features.box.name')} </h3>
-                
+                    <h3 className="text-5xl font-bold text-indigo-600"> {t('features.box.name')} </h3>
                   </div>
-                  <p className="mt-5 text-md text-gray-500  "> {t('features.box.description')} </p>
+                  <ul className="mt-5 ml-5 text-lg text-gray-500">
+                    <li className="list-disc">{t('features.box.line-1')}</li>
+                    <li className="list-disc">{t('features.box.line-2')}</li>
+                    <li className="list-disc">{t('features.box.line-3')}</li>
+                    <li className="list-disc	 ">{t('features.box.line-4')}</li>
+                    <li className="list-disc	 ">{t('features.box.line-5')}</li>
+                  </ul>
+                  {/* <p className="mt-5 text-md text-gray-500  "> {t('features.box.description')} </p> */}
                 </div>
                 <div className="flex flex-row mt-10 ">
-                <dl className="  text-gray-900 ">
+                  <dl className="  text-gray-900 ">
                     <div className="flex flex-col  lg:items-center gap-y-3 ">
                       <VideoButton videoSelector="videos/box.mp4" />
-                      <dt className="text-sm leading-6 text-indigo-600 font-bold ">{t('videocta')} </dt>
+                      <dt className="text-xs lg:text-sm lg:leading-6 leading-1  text-indigo-600 font-bold ">{t('videocta')} </dt>
                     </div>
                   </dl>
                   <dl className=" lg:ml-10 ml-5 text-gray-900 ">
                     <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
-                      <dt className="text-sm leading-6">{t('features.box.stat-1-description')} </dt>
-                      <dd className="order-first text-3xl font-semibold tracking-tight">{t('features.box.stat-1')}</dd>
+                      <dt className=" text-xs lg:text-sm    leading-4 leading-1">{t('features.box.stat-1-description')} </dt>
+                      <dd className="order-first text-2xl lg:text-3xl font-semibold tracking-tight">{t('features.box.stat-1')}</dd>
                     </div>
                   </dl>
                   <dl className="lg:ml-10 ml-5 text-gray-900 ">
-                    <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
-                      <dt className="text-sm leading-6">{t('features.box.stat-2-description')} </dt>
-                      <dd className="order-first text-3xl font-semibold tracking-tight">{t('features.box.stat-2')}</dd>
+                  <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
+                      <dt className=" text-xs lg:text-sm    leading-4 leading-1">{t('features.box.stat-2-description')} </dt>
+                      <dd className="order-first text-2xl lg:text-3xl font-semibold tracking-tight">{t('features.box.stat-2')}</dd>
                     </div>
                   </dl>
                 </div>
@@ -106,32 +120,33 @@ export default function Solution() {
               <div className="flex flex-col max-w-3xl">
                 <div className="flex flex-col">
                   <div className="flex flex-row">
-                    <h3 className="text-3xl font-bold text-indigo-600"> {t('features.handheld.name')} </h3>
-               
+                    <h3 className="text-5xl font-bold text-indigo-600"> {t('features.handheld.name')} </h3>
                   </div>
-                  <p className="mt-5 text-md text-gray-500 "> {t('features.handheld.description')} </p>
+                  <ul className="mt-5 ml-5 text-lg text-gray-500">
+                    <li className="list-disc">{t('features.handheld.line-1')}</li>
+                    <li className="list-disc">{t('features.handheld.line-2')}</li>
+                    <li className="list-disc">{t('features.handheld.line-3')}</li>
+                    <li className="list-disc	 ">{t('features.handheld.line-4')}</li>
+                
+                  </ul>
                 </div>
-                <div className="flex flex-row mt-10  ">
-                <dl className="  text-gray-900 ">
+                <div className="flex flex-row mt-10 ">
+                  <dl className="  text-gray-900 ">
                     <div className="flex flex-col  lg:items-center gap-y-3 ">
                       <VideoButton videoSelector="videos/handheld.mp4" />
-                      <dt className="text-sm leading-6 text-indigo-600 font-bold ">{t('videocta')} </dt>
+                      <dt className="text-xs lg:text-sm lg:leading-6 leading-1  text-indigo-600 font-bold ">{t('videocta')} </dt>
                     </div>
                   </dl>
                   <dl className=" lg:ml-10 ml-5 text-gray-900 ">
                     <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
-                      <dt className="text-sm leading-6">{t('features.handheld.stat-1-description')} </dt>
-                      <dd className="order-first text-3xl font-semibold tracking-tight">
-                        {t('features.handheld.stat-1')}
-                      </dd>
+                      <dt className=" text-xs lg:text-sm    leading-4 leading-1">{t('features.handheld.stat-1-description')} </dt>
+                      <dd className="order-first text-2xl lg:text-3xl font-semibold tracking-tight">{t('features.handheld.stat-1')}</dd>
                     </div>
                   </dl>
                   <dl className="lg:ml-10 ml-5 text-gray-900 ">
-                    <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
-                      <dt className="text-sm leading-6">{t('features.handheld.stat-2-description')} </dt>
-                      <dd className="order-first text-3xl font-semibold tracking-tight">
-                        {t('features.handheld.stat-2')}
-                      </dd>
+                  <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
+                      <dt className=" text-xs lg:text-sm    leading-4 leading-1">{t('features.handheld.stat-2-description')} </dt>
+                      <dd className="order-first text-2xl lg:text-3xl font-semibold tracking-tight">{t('features.handheld.stat-2')}</dd>
                     </div>
                   </dl>
                 </div>
@@ -148,28 +163,33 @@ export default function Solution() {
               <div className="flex flex-col max-w-3xl">
                 <div className="flex flex-col">
                   <div className="flex flex-row">
-                    <h3 className="text-3xl font-bold text-indigo-600"> {t('features.pos.name')} </h3>
-                   
+                    <h3 className="text-5xl font-bold text-indigo-600"> {t('features.pos.name')} </h3>
                   </div>
-                  <p className="mt-5 text-md text-gray-500  "> {t('features.pos.description')} </p>
+                  <ul className="mt-5 ml-5 text-lg text-gray-500">
+                    <li className="list-disc">{t('features.pos.line-1')}</li>
+                    <li className="list-disc">{t('features.pos.line-2')}</li>
+                    <li className="list-disc">{t('features.pos.line-3')}</li>
+                    <li className="list-disc	 ">{t('features.pos.line-4')}</li>
+                    <li className="list-disc	 ">{t('features.pos.line-5')}</li>
+                  </ul>
                 </div>
                 <div className="flex flex-row mt-10 ">
-                <dl className="  text-gray-900 ">
+                  <dl className="  text-gray-900 ">
                     <div className="flex flex-col  lg:items-center gap-y-3 ">
                       <VideoButton videoSelector="videos/pos.mp4" />
-                      <dt className="text-sm leading-6 text-indigo-600 font-bold ">{t('videocta')} </dt>
+                      <dt className="text-xs lg:text-sm lg:leading-6 leading-1  text-indigo-600 font-bold ">{t('videocta')} </dt>
                     </div>
                   </dl>
                   <dl className=" lg:ml-10 ml-5 text-gray-900 ">
                     <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
-                      <dt className="text-sm leading-6">{t('features.pos.stat-1-description')} </dt>
-                      <dd className="order-first text-3xl font-semibold tracking-tight">{t('features.pos.stat-1')}</dd>
+                      <dt className=" text-xs lg:text-sm    leading-4 leading-1">{t('features.pos.stat-1-description')} </dt>
+                      <dd className="order-first text-2xl lg:text-3xl font-semibold tracking-tight">{t('features.pos.stat-1')}</dd>
                     </div>
                   </dl>
                   <dl className="lg:ml-10 ml-5 text-gray-900 ">
-                    <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
-                      <dt className="text-sm leading-6">{t('features.pos.stat-2-description')} </dt>
-                      <dd className="order-first text-3xl font-semibold tracking-tight">{t('features.pos.stat-2')}</dd>
+                  <div className="flex flex-col gap-y-3 border-l border-gray-900 pl-6">
+                      <dt className=" text-xs lg:text-sm    leading-4 leading-1">{t('features.pos.stat-2-description')} </dt>
+                      <dd className="order-first text-2xl lg:text-3xl font-semibold tracking-tight">{t('features.pos.stat-2')}</dd>
                     </div>
                   </dl>
                 </div>
